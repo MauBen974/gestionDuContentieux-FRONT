5d3f6b19b1d2d7a76cd7347aa9b5f2cf69dba13c
Guillaume VITTOZ <vittoz.guillaume@live.fr>
2020-11-25 14:30:37 +0100

    début tache component

--- src/app/tache/tache.component.ts
+++ src/app/tache/tache.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Phase } from 'app/model/phase';
+import { Tache } from 'app/model/tache';
+import { PhaseService } from 'app/service/phase.service';
+import { TacheService } from 'app/service/tache.service';
 
 @Component({
   selector: 'app-tache',
@@ -7,9 +11,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TacheComponent implements OnInit {
 
-  constructor() { }
+  idTache: number;
+  idPhase: number;
+  libellePhase : String;
+  statusAudience : Boolean;
+  idTribunal: number;
+
+  taches: Tache[];
+  tache: Tache = new Tache();
+  phases: Phase[];
+  phase: Phase = new Phase();
+
+  constructor(private tacheService: TacheService, private phaseService: PhaseService) { }
 
   ngOnInit() {
+    // this.findByIdUtilisateur();
+    this.findAllTache();
   }
 
+  // findByIdUtilisateur(){
+  //   this.tacheService.findByIdUtilisateur(idARECUP).subscribe();
+  // }
+
+  findAllTache() {
+    this.tacheService.getAll().subscribe();
+  }
+
+  findAllPhase() {
+    this.phaseService.getAll().subscribe();
+  }
+
+  deleteTache(tache) {
+    this.tacheService.delete(tache.id).subscribe(() => { this.findAllTache() });
+  }
+
+  deletePhase(phase) {
+    this.phaseService.delete(phase.id).subscribe(() => { this.findAllPhase() });
+  }
+
+  saveTache() {
+    this.tacheService.save(this.tache).subscribe(() => {
+      this.findAllTache();
+      this.tache = new Tache();
+    }
+    )
+  }
+  savePhase() {
+    this.phaseService.save(this.phase).subscribe(() => {
+      this.findAllTache();
+      this.phase = new Phase();
+    }
+    )
+  }
+
+  updateTache(){
+    this.tacheService.update(this.idTache, this.tache).subscribe(()=>{
+      this.findAllTache();
+    })
+  }
+
+  updateLibelle(){
+    this.phaseService.updateLibelle(this.idPhase, this.phase).subscribe(()=>{
+      this.findAllTache();
+    })
+  }
+
+  findBylibellePhase(){
+    this.tacheService.findBylibellePhase(this.libellePhase).subscribe();
+  }
+
+  findByStatusAudience(){
+    this.tacheService.findByStatusAudience(this.statusAudience).subscribe();
+  }
+
+  findByIdTribunal(){
+    this.tacheService.findByIdTribunal(this.idTribunal).subscribe();
+  }
+
+  getAllNotTermined(){
+    this.phaseService.getAllNotTermined().subscribe();
+  }
+
+  findByTache(){
+    this.phaseService.findByTache(this.tache).subscribe();
+  }
 }
+
+
